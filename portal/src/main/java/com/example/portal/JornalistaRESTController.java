daf1ec43317199ca88dde256f93ce447a2aed2a7
Pedro Diogo <pedrodiogo219@gmail.com>
2022-08-22 15:47:46 -0300

    Allowing CORS

--- portal/src/main/java/com/example/portal/JornalistaRESTController.java
+++ portal/src/main/java/com/example/portal/JornalistaRESTController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ public class JornalistaRESTController {
     @Autowired
     private JornalistaRepository jornalistaRepository;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public List<Jornalista> findAllUsers() {
         // Implement
